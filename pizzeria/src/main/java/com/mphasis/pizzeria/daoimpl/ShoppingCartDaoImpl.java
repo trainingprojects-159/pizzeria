c381bc26cdc2978b2dc60086ce8641aeae9bdd4c
amit.sahu <amit.sahu@WKSBAN24TR17015.corp.mphasis.com>
2019-05-09 10:27:21 +0530

    cart and topping features addedd

---
+++ pizzeria/src/main/java/com/mphasis/pizzeria/daoimpl/ShoppingCartDaoImpl.java
@@ -0,0 +1,55 @@
+package com.mphasis.pizzeria.daoimpl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mphasis.pizzeria.daos.ShoppingCartDao;
+import com.mphasis.pizzeria.entities.ShoppingCart;
+
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+	@Autowired
+	 SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory)
+	{
+		this.sessionFactory=sessionFactory;
+	}	
+	public void insertToCart(ShoppingCart shoppingcart) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+  System.out.println(shoppingcart.getQuantity()+"shoppingcart values in dao");
+  session.save(shoppingcart);
+  tr.commit();
+
+	}
+
+	public void updateToCart(ShoppingCart shoppingcart) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		  session.update(shoppingcart);
+		  tr.commit();
+
+	}
+
+	public void deleteToCart(String cartid) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		ShoppingCart shoppingcart=(ShoppingCart) session.get(ShoppingCart.class, cartid);
+		  session.delete(shoppingcart);
+		  tr.commit();
+
+	}
+
+	public List<ShoppingCart> getAllCarts() {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		 List<ShoppingCart> shoppingcart = session.createCriteria(ShoppingCart.class).list();
+		  tr.commit();
+		return null;
+	}
+
+}
