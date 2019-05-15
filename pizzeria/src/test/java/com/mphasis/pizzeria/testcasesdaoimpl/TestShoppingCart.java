9965ea6bcc08fbc792eedac4e8ce9bd0f6bdc32a
mounika.naidu <mounika.naidu@WKSBAN24TR17018.corp.mphasis.com>
2019-05-15 18:55:26 +0530

    added junit file

---
+++ pizzeria/src/test/java/com/mphasis/pizzeria/testcasesdaoimpl/TestShoppingCart.java
@@ -0,0 +1,14 @@
+package com.mphasis.pizzeria.testcasesdaoimpl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestShoppingCart {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
